032b0dbde270edd5e8a7152f13cf82400ca03dbe
Shivam <exceptionunique@gmail.com>
2023-05-14 17:42:46 +0530

    todo-app

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const app = express();
+
+app.use(express.static("public"));
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+let newItems = []
+app.get('/', (req,res) => {
+    let options = { weekday:'long',year:'numeric',month:'long',
+                    day:'numeric'};
+    let today = new Date();
+    let day = today.toLocaleDateString("en-US", options);
+    res.render("list", {kindOfDay:day, newListItems: newItems});
+});
+
+app.post('/',(req,res) => {
+ let newItem =  req.body.newItem;
+ newItems.push(newItem);
+  res.redirect('/');
+});
+
+
+
+app.listen(3000, () => console.log("port is running at server 3000"));
